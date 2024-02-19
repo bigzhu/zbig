d63fbf11b7ea8aa13a1272d93675e684033c3362
bigzhu <vermiliondun@gmail.com>
2024-02-19 16:53:03 +0800

    add test now

--- tests/test_time.py
+++ tests/test_time.py
@@ -1,5 +1,11 @@
-from bigzhu_py.time import now_print
+from bigzhu_py.time import now_print, now
 import re
+
+
+def test_now():
+    now_str = now()
+    assert bool(
+        re. match(r'^(\d{4}-\d{2}-\d{2})\s+(\d{2}:\d{2}:\d{2})$', now_str))
 
 
 def test_now_print(capfd):
