361a319c7af084630e3dd188899ee878d8d3d9bf
bigzhu <vermiliondun@gmail.com>
2024-03-14 14:20:11 +0800

    q

--- zbig/zprint/__init__.py
+++ zbig/zprint/__init__.py
@@ -3,8 +3,9 @@ Some tool
 
 Print python library.
 """
+
 from .cn_now import cn_now
-from .table import table
+from .table import table, curses_table
 from .json_print import json_print
 
 __version__ = "0.1.0"
--- zbig/zprint/table.py
+++ zbig/zprint/table.py
@@ -1,7 +1,9 @@
 import unicodedata
-
+import curses
 
 # 算出有几个非英文字符
+
+
 def wide_chars(s):
     return sum(unicodedata.east_asian_width(x) == "W" for x in s)
 
@@ -39,16 +41,41 @@ def table(rows: list, spliter: str):
         print(spliter.join(formated_row))
 
 
+# 刷新绘制, 不会重复输出表格内容
+def curses_table(rows: list, spliter: str):
+    stdscr = curses.initscr()
+    # Clear screen
+    stdscr.clear()
+    max_lens = get_max_lens(rows)
+    for row in rows:
+        # formated_row = [str(row[i]).ljust(max_lens[i]) for i in range(len(row))]
+        # ljust 使用 len 判断长度, 支持非英文字符, 导致对每个非英文字符多填充了一个空白
+        # 解决办法是算出有n个非英字符, just 长度-n
+        formated_row = [
+            str(row[i]).ljust(max_lens[i] - wide_chars(str(row[i])))
+            for i in range(len(row))
+        ]
+        stdscr.addstr(spliter.join(formated_row) + "\n")
+
+    stdscr.refresh()
+    stdscr.getkey()
+
+
 if __name__ == "__main__":
-    data = [
-        ["用户名", "服务器地址", "服务器的一些说明"],
-        ["用户名User", "Host", "Description"],
-        ["User", "Host", "Description"],
-        ["root", "h.bigzhu.net", "dump"],
-        ["root", "racknerd.bigzhu.net", "racknerd"],
-        ["bigzhu", "ssh.entube.app", "digitalocean"],
-    ]
-    table(data, " ")
+    import time
+
+    # table(data, " ")
+    while True:
+        data = [
+            ["用户名", "服务器地址", "服务器的一些说明"],
+            ["用户名User", f"{time.time()}", "Description"],
+            ["User", "Host", "Description"],
+            ["root", "h.bigzhu.net", "dump"],
+            ["root", "racknerd.bigzhu.net", "racknerd"],
+            ["bigzhu", "ssh.entube.app", "digitalocean"],
+        ]
+        curses_table(data, "  ")
+        time.sleep(2)
     # i = "服务器地址什么"
     # j = "qwertyuiop"
     # print(width(i))
