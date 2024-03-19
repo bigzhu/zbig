de904490b327abc42f70c1b3de0cc1f395dc379b
bigzhu <vermiliondun@gmail.com>
2024-03-19 14:04:19 +0800

    add doctest

--- zbig/zhash/args.py
+++ zbig/zhash/args.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import hashlib
 
 
@@ -12,9 +13,17 @@ def args_hash(*args, **kw):
     Given the same positional arguments and keyword
     arguments, this function will produce the same
     hash key.
+    >>> args_hash(name='bigzhu', age='18')
+    '5505ff0b8f82e73373f86c22dcd91efdd59dcafb'
     """
     items = list(args) + [i for t in sorted(kw.items()) for i in t]
     items = ("__NoneType__" if _i is None else _i for _i in items)
     # All items must be strings
     args_string = "|".join(items)
     return hashlib.sha1(_tob(args_string)).hexdigest()
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=False, optionflags=doctest.ELLIPSIS)
--- zbig/ztelegram/send_photo.py
+++ zbig/ztelegram/send_photo.py
@@ -1,10 +1,22 @@
-from .define import bot, CHAT_ID
+#!/usr/bin/env python
+
+from define import bot, CHAT_ID
 
 
 def send_photo(file_path: str, caption: str):
-    photo = open(file_path, 'rb')
+    """
+
+    Args:
+        file_path:
+        caption:
+
+    >>> send_photo('/home/bigzhu/Pictures/00006.jpeg', 'test')
+    """
+    photo = open(file_path, "rb")
     bot.send_photo(chat_id=CHAT_ID, photo=photo, caption=caption)
 
 
 if __name__ == "__main__":
-    send_photo('/home/bigzhu/Pictures/00006.jpeg', 'test')
+    import doctest
+
+    doctest.testmod(verbose=False, optionflags=doctest.ELLIPSIS)
